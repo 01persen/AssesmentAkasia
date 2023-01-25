b8b1fc8c87a860a45c25914d71e473cc7d8ea644
Perdana Sendy <74180064+01persen@users.noreply.github.com>
2023-01-25 11:09:54 +0700

    Add files via upload

---
+++ TestAssessmentAkasia/BusinessLogic/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BusinessLogic
+{
+    public class Logic
+    {
+        public string Proses(int i)
+        {
+            string result = string.Empty;
+            int k;
+            if (i%3 == 0 && i % 5 == 0)
+            {
+                result = "FizzBuzz";
+            }
+            else if (i % 5 == 0)
+            {
+                result = "Buzz";
+            }else if(i % 3 == 0)
+            {
+                result = "Fizz";
+            }
+            else
+            {
+                result = i.ToString();
+            }
+            return result;
+        }
+    }
+}
---
+++ TestAssessmentAkasia/TestAssessmentAkasia/Program.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLogic;
+using System;
+
+namespace TestAssessmentAkasia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logic oLogic = new Logic();
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(oLogic.Proses(i));
+            }
+            
+        }
+    }
+}
